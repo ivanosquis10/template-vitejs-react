27cad18ca1207ef9c1b79139fe2e77a4925ad9fc
Ivan Rodriguez <ivanosquis02.08@gmail.com>
2023-04-23 15:30:41 -0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div>editame</div>
+    </>
+  )
+}
+
+export default App
